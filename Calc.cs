b4b67876069d719fa7ca3e3e1e88f60d1f6537d3
Tobias Bachmann <bachmanntobias96@gmail.com>
2018-03-15 14:34:21 +0100

    Signed-off-by: Tobias Bachmann <bachmanntobias96@gmail.com>
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch master
     Changes to be committed:
    	new file:   Calc.cs

---
+++ Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Calc
+    {
+        public Calc()
+        {
+            
+        }
+
+        public static void Calcer()
+        {
+            Console.WriteLine("TEST");
+
+
+
+            Program.Starter();
+        }
+
+
+    }
+}
